4df5df385ede9fc6f6ff222ae03be1a9be3a66b5
stormzy22 <aawweb23@gmail.com>
2022-03-07 01:12:39 +0100

    --error check

--- clear.sh
+++ clear.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo "" > "result/arp-scan.txt"
-
-sudo rm -rfv store/csv/* && sudo rm -rfv _gateway/* && sudo rm -rfv store/txt/* 
+sudo rm -rfv store/csv/* && sudo rm -rfv _gateway/* && sudo rm -rfv store/txt/* && sudo rm -rfv result/arp-scan.txt
--- clear_white_list.sh
+++ clear_white_list.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-echo "" > "white_list/list.txt"
-echo "" > "result/arp-scan.txt"
-sudo rm -rfv store/csv/* && sudo rm -rfv _gateway/* && sudo rm -rfv store/txt/* 
+./clear.sh
+sudo rm -rfv white_list/list.txt
